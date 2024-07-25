fd8af4a22a7f45e39b81767e3e710590f38619ea
Joseph Simon <josephksimon@gmail.com>
2024-07-25 16:53:17 +0530

    Cart number DOT function

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,95 @@
+import React, { useContext, useEffect, useState } from 'react'
+import "./Navbar.css"
+import { Container,Navbar, Nav, Dropdown, Image } from 'react-bootstrap';
+import { assets } from '../../assets/assets';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { StoreContext } from '../../Context/StoreContext';
+
+
+
+const Header = () => {
+
+ 
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState(null);
+  
+    useEffect(() => {
+      const storedUser = sessionStorage.getItem('Existinguser');
+      if (storedUser) {
+        try {
+          setUserData(JSON.parse(storedUser));
+        } catch (error) {
+          console.error('Failed to parse user data from session storage', error);
+        }
+      }
+    }, []);
+  
+    const logout = () => {
+      sessionStorage.removeItem('Existinguser');
+      sessionStorage.removeItem('token');
+      sessionStorage.removeItem('email');
+      sessionStorage.removeItem('phone');
+      navigate('/');
+    };
+  
+    const defaultImage = assets.profileicon
+
+    const{getTotalCartAmount} =useContext(StoreContext)
+
+  return (
+    <div>
+         <Navbar expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#home" className="ms-3">
+            <img src={assets.logo} alt="Logo" style={{ height: '75px' }} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto ">
+              <Nav.Link href="#home" className="text-black me-5 ms-2 hover fw-bold ">Home</Nav.Link>
+              <Nav.Link href="#explore-menu" className="text-black me-5 ms-2 hover fw-bold">Menu</Nav.Link>
+              <Nav.Link href="#footer" className="text-black me-5 ms-2 hover fw-bold">Contact Us</Nav.Link>
+             <div className='navcart '>
+                
+                    <Nav.Link as={NavLink} to={'/cart'}   className="me-5 hover ms-2 ">
+                      <img src={assets.carticon} alt="" height="20px" width="20px" />
+                    </Nav.Link>
+                 
+                
+                <div className={getTotalCartAmount()===0?"":"dot"}></div>
+             </div>
+              {userData ? (
+                <Dropdown className="me-5 mt-1">
+                  <Dropdown.Toggle variant="link" id="dropdown-basic" className="d-flex align-items-center text-decoration-none">
+                    <Image
+                      src={userData.image || defaultImage}
+                      alt="User Avatar"
+                      height="15px"
+                      width="15px"
+                      roundedCircle
+                      className="me-3"
+                    />
+
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu align="end">
+                    <Dropdown.Item>
+                      <Image
+                        src={userData.image || defaultImage} alt="User Avatar" height="15px" width="15px" roundedCircle className="mx-auto mt-2 mb-1" />
+                    </Dropdown.Item>
+                    <Dropdown.Item> {userData.username} </Dropdown.Item>
+                    <Dropdown.Item >{userData.email}</Dropdown.Item>
+                    <Dropdown.Item >{userData.phone}</Dropdown.Item>
+                    <Dropdown.Item onClick={logout} style={{ color: "#FE0C00" }}><i class="fa-solid fa-arrow-right-from-bracket me-1"></i>Logout</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              ) : null}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Header
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,114 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Navbar, Nav, Dropdown, Image } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { assets } from '../../assets/assets';
-import { useNavigate } from 'react-router-dom';
-import './Home.css';
+import React, { useState } from 'react';
 import HomeCarousal from '../../Components/Home Carousal/HomeCarousal';
 import MenuExplore from '../../Components/Menu Explore/MenuExplore';
 import Footer from '../../Components/Footer/Footer';
 import AppDownload from '../../Components/App Download/AppDownload';
 import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay';
+import Header from '../../Components/Navbar/Navbar';
 
 
 
 
 const Home = () => {
 
-const [category,setCategory]= useState("All")
-
-
-
-
-
-
-
-  const navigate = useNavigate();
-  const [userData, setUserData] = useState(null);
-
-  useEffect(() => {
-    const storedUser = sessionStorage.getItem('Existinguser');
-    if (storedUser) {
-      try {
-        setUserData(JSON.parse(storedUser));
-      } catch (error) {
-        console.error('Failed to parse user data from session storage', error);
-      }
-    }
-  }, []);
-
-  const logout = () => {
-    sessionStorage.removeItem('Existinguser');
-    sessionStorage.removeItem('token');
-    sessionStorage.removeItem('email');
-    sessionStorage.removeItem('phone');
-    navigate('/');
-  };
-
-  const defaultImage = assets.profileicon// Default image URL
+  const [category, setCategory] = useState("All")
 
   return (
     <div>
-      <Navbar expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#home" className="ms-3">
-            <img src={assets.logo} alt="Logo" style={{ height: '75px' }} />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto ">
-              <Nav.Link href="#home" className="text-black me-5 ms-2 hover fw-bold ">Home</Nav.Link>
-              <Nav.Link href="#explore-menu" className="text-black me-5 ms-2 hover fw-bold">Menu</Nav.Link>
-              <Nav.Link href="#footer" className="text-black me-5 ms-2 hover fw-bold">Contact Us</Nav.Link>
-             <div className='navcart'>
-                <Nav.Link href="Cart" className="me-5 hover ms-2">
-                  <img src={assets.carticon} alt="" height="20px" width="20px" />
-                </Nav.Link>
-                <div className='dot'></div>
-             </div>
-              {userData ? (
-                <Dropdown className="me-5 mt-1">
-                  <Dropdown.Toggle variant="link" id="dropdown-basic" className="d-flex align-items-center text-decoration-none">
-                    <Image
-                      src={userData.image || defaultImage}
-                      alt="User Avatar"
-                      height="15px"
-                      width="15px"
-                      roundedCircle
-                      className="me-3"
-                    />
 
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu align="end">
-                    <Dropdown.Item>
-                      <Image
-                        src={userData.image || defaultImage} alt="User Avatar" height="15px" width="15px" roundedCircle className="mx-auto mt-2 mb-1" />
-                    </Dropdown.Item>
-                    <Dropdown.Item> {userData.username} </Dropdown.Item>
-                    <Dropdown.Item >{userData.email}</Dropdown.Item>
-                    <Dropdown.Item >{userData.phone}</Dropdown.Item>
-                    <Dropdown.Item onClick={logout} style={{ color: "#FE0C00" }}><i class="fa-solid fa-arrow-right-from-bracket me-1"></i>Logout</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              ) : null}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      {/* ----------------------------------------------------------------------------------- */}
-     <HomeCarousal></HomeCarousal>
-
-     <MenuExplore category={category} setCategory={setCategory}></MenuExplore>
-     <FoodDisplay category={category}></FoodDisplay>
-
-      
-
+      <Header></Header>
+      <HomeCarousal></HomeCarousal>
+      <MenuExplore category={category} setCategory={setCategory}></MenuExplore>
+      <FoodDisplay category={category}></FoodDisplay>
       <AppDownload></AppDownload>
-
       <Footer></Footer>
 
-      
+
 
 
 
